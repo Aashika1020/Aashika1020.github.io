686d6613307dad69f8f83b29dc274ce557c7a0e3
Aashika1020 <125243238+aaj2002@users.noreply.github.com>
2023-10-17 15:03:56 +0400

    lab-3Interactive Gallery Slideshow

--- week3/javascript.js
+++ week3/javascript.js
@@ -1,20 +1,33 @@
 let prevBtn = document.getElementsByClassName("prevBtn");
 let nextBtn = document.getElementsByClassName("nextBtn");
 var current = 0; //Current Slideshow Image Index
-var slideshow_img = document.querySelector("slideshow-img");
+var slideshow_img = document.querySelector("slideshow-img img");
 var images= ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'];
 
 
 prevBtn.onlick() = function(){
-    if(current<=0)
+    if(current<=0){
+        current = images.length - 1;
         current--;
-        return setSlide();
+    }
+    else {
+        current--;
+    }
+    setSlide();
+    
 }
 nextBtn.onlick() = function(){
-    current++;
-    return setSlide();
+    if(current>=images.length-1){
+        current=0;
+    }
+    else{
+        current++;
+    }
+    setSlide();
 }
 
 function setSlide(){
     return slideshow_img.setAttribute('src', 'images/'+images[current]);
 }
+
+setSlide(); // Display the initial image
